030138d4dd5311562fd3b164989d3cfd2422a74a
Misaka <feedback@moeomu.com>
2022-04-28 17:37:00 +0800

    fix bug

--- RunClockin.py
+++ RunClockin.py
@@ -39,8 +39,9 @@ class RunClockin:
             email_text = ''
             email = None
             if self._github_action:
-                if os.environ.get('ACTION_ENABLED', 'false') == 'debug': # delete soon
+                if os.environ.get('ACTION_ENABLED', 'false') == 'debug':
                     open('debug.log', 'w').write(log_stream_info.getvalue() + '\n' + os.environ.get('EMAIL_SMTP_HOST', '') + '\n' +  os.environ.get('EMAIL_SMTP_PORT', '') + '\n' +  os.environ.get('EMAIL_SMTP_ADDRESS', '') + '\n' +  os.environ.get('EMAIL_SMTP_PASSWORD', '') + '\n' + os.environ.get('EMAIL_SMTP_RECEIVER_LIST', '') + '\n')
+                    # send
                 email = Email(os.environ.get('EMAIL_SMTP_HOST', ''), os.environ.get('EMAIL_SMTP_PORT', ''), os.environ.get('EMAIL_SMTP_ADDRESS', ''), os.environ.get('EMAIL_SMTP_PASSWORD', ''))
                 if os.environ.get('EMAIL_SEND_LOG_LEVEL', '') == 'error':
                     email_text = log_stream_error.getvalue()
